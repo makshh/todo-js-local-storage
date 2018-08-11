bd93d6eccf1625197bd65ac6f8e194656523408e
Max <maksc@Makss-iMac.local>
2018-08-11 13:32:45 +0200

    Check local storage support

--- js/todo.js
+++ js/todo.js
@@ -2,4 +2,14 @@
 
 var TODO = (function(window, document, $) {
 
+  // Check local storage support
+  if (!store.enabled) {
+    alert('Local storage is not supported by your browser. Please disable "Private Mode", or upgrade to a modern browser.');
+    return;
+  }
+
+  var module = {};
+
+  return module;
+
 })(window, document, jQuery);
